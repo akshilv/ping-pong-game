594f9225d1dd7eaae2533af8ded6c821690a15fd
Akshil Verma <akshilverma4@gmail.com>
2019-04-17 14:24:33 +0530

    Add net and fix JSDocs
    - Add a function to create a dashed line
    - Create a net using the dashed line function
    - Fix the JSDocs parameters

--- gameController.js
+++ gameController.js
@@ -1,7 +1,9 @@
-// Get the canvas and context element
+// Canvas related globals
 const canvas = document.getElementById('gameCanvas');
 const canvasContext = canvas.getContext('2d');
+const canvasColor = 'blue';
 // Paddle related globals
+const paddleColor = 'white';
 const paddleHeight = 100;
 const paddleWidth = 10;
 var yUserPaddle = canvas.height/2 - paddleHeight/2;
@@ -9,11 +11,16 @@ var yAIPaddle = canvas.height/2 - paddleHeight/2;
 var yAIPaddleSpeed = 6;
 const yAIPaddleIgnorableArea = paddleHeight*0.35;
 // Ball related globals
+const ballColor = 'white';
 const ballRadius = 10;
 var xBall = canvas.width/2 - ballRadius/2;
 var yBall = canvas.height/2 - ballRadius/2;
 var xBallSpeed = 5;
 var yBallSpeed = 5;
+// Net related globals
+const netWidth = 5;
+const netColor = 'white';
+const netPattern = [35];
 // Frames per second
 const fps = 30;
 // Points
@@ -32,16 +39,18 @@ window.onload = function () {
     // Draw everything based on fps
     setInterval(function () {
         // Draw the canvas
-        drawRect(0, 0, canvas.width, canvas.height, 'blue');
+        drawRect(0, 0, canvas.width, canvas.height, canvasColor);
         // Draw paddles
-        drawRect(0, yUserPaddle - paddleHeight/2, paddleWidth, paddleHeight, 'white');        // Center the paddle around the mouse pointer
-        drawRect(canvas.width - paddleWidth, yAIPaddle, paddleWidth, paddleHeight, 'white');
+        drawRect(0, yUserPaddle - paddleHeight/2, paddleWidth, paddleHeight, paddleColor);        // Center the paddle around the mouse pointer
+        drawRect(canvas.width - paddleWidth, yAIPaddle, paddleWidth, paddleHeight, paddleColor);
+        // Draw the net
+        drawDashedLine(canvas.width/2, 0, canvas.width/2, canvas.height, netPattern, netWidth, netColor);
         // Show score
         canvasContext.font = '30px Arial';
         canvasContext.fillText(userPoints, canvas.width/4, canvas.height/2);
         canvasContext.fillText(AIPoints, canvas.width*3/4, canvas.height/2);
         // Draw the ball
-        drawCircle(ballRadius, 'white');
+        drawCircle(ballRadius, ballColor);
         // Move the ball
         moveBall();
         // Move the AI Paddle
@@ -51,15 +60,35 @@ window.onload = function () {
 
 /**
  * Function to draw and fill a rectangle
- * @param {Number} xValue 
- * @param {Number} yValue 
- * @param {Number} width 
- * @param {Number} height 
- * @param {String} color 
+ * @param {Number} xValue - Distance from x coordinate of canvas
+ * @param {Number} yValue - Distance from y coordinate of canvas
+ * @param {Number} width - Width of rectange
+ * @param {Number} height - Height of rectangle
+ * @param {String} color - Color of rectange
  */
 function drawRect (xValue, yValue, width, height, color) {
     canvasContext.fillStyle = color;
     canvasContext.fillRect(xValue, yValue, width, height);
+}
+
+/**
+ * Function to draw a fill a dashed line
+ * @param {Number} xStart - Starting x coordinate of line
+ * @param {Number} yStart - Starting y coordinate of line
+ * @param {Number} xEnd - Ending x coordinate of line
+ * @param {Number} yEnd - Ending x coordinate of line
+ * @param {Array} pattern - Pattern of dashed line
+ * @param {Number} width - Width of line
+ * @param {String} color - Color of line
+ */
+function drawDashedLine(xStart, yStart, xEnd, yEnd, pattern, width, color) {
+    canvasContext.beginPath();
+    canvasContext.setLineDash(pattern);
+    canvasContext.moveTo(xStart, yStart);
+    canvasContext.lineTo(xEnd, yEnd);
+    canvasContext.strokeStyle = color;
+    canvasContext.lineWidth = width;
+    canvasContext.stroke();
 }
 
 /**
@@ -119,7 +148,7 @@ function reset () {
 
 /**
  * Returns the mouse position on canvas
- * @param {Event} event 
+ * @param {Event} event - The triggered event 
  */
 function getMousePos (event) {
     var rect = canvas.getBoundingClientRect();
